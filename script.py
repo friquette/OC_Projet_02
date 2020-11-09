fdbea667d45c0a2a1342d3821585fd6e6fa202d2
Thomas Fricard <thomasfricard@gmail.com>
2020-11-09 15:57:36 +0100

    Troisieme partie du projet 02: extension du script au site en entier avec écriture d'un fichier csv par catégorie et création d'une fonction pour la récupération du contenu d'un produit

--- script.py
+++ script.py
@@ -1,68 +1,90 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-import time
 
-i = 0
-links = []
 
-while True:
-    i += 1
-    urls = 'http://books.toscrape.com/catalogue/category/books/fantasy_19/page-' + str(i) + '.html'
-    response = requests.get(urls)
+def getContent():
+    for urlBook in urlBooks:
+        aBooks = urlBook.find('a')
+        linkBook = aBooks['href']
+        linkBooks = url + 'catalogue/' + linkBook[9:]
 
-    if response.ok:
-        soup = BeautifulSoup(response.text, 'html.parser')
+        responseContent = requests.get(linkBooks)
+        soupContent = BeautifulSoup(responseContent.text, 'html.parser')
 
-        urlBooks = soup.findAll('div', {'class': 'image_container'})
-        classNext = soup.find('li', {'class': 'next'})
+        trs = soupContent.findAll('tr')
+        upc = trs[0].find('td')
+        priceIncl = trs[3].find('td')
+        priceExcl = trs[2].find('td')
 
-        for urlBook in urlBooks:
-            a = urlBook.find('a')
-            link = a['href']
-            links.append('http://books.toscrape.com/catalogue/' + link[9:])
+        nbrAvailable = trs[5].find('td')
+        nbr = int(''.join(filter(str.isdigit, nbrAvailable.text)))
 
-    if not response.ok:
-        break
+        title = soupContent.find('h1')
+        description = soupContent.find('p', {'class': ''})
 
-with open('urlsBooks.csv', 'w') as outp:
-    books = csv.writer(outp, delimiter=',')
-    for link in links:
-        outp.write(link + '\n')
+        bookCategory = soupContent.findAll('li')[2]
+        rating = soupContent.find('p', {'class': 'star-rating'}).get('class')
 
-with open('urlsBooks.csv', 'r') as inf:
-    with open('books.csv', 'w', encoding='ISO-8859-1') as outf:
-        booksFile = csv.writer(outf, delimiter=',', quoting=csv.QUOTE_NONE, quotechar="")
+        urlImgRaw = soupContent.find('div', {'class': 'item active'}).find('img')
+        urlImg = urlImgRaw.get('src')[6:]
 
-        outf.write('product_page_url, universal_product_code, title, '
-                   'price_including_tax, price_excluding_tax, number_available,'
-                   'product_description, category, review_rating, image_url\n')
+        outp.write(linkBooks + ',' + upc.text + ',' + '"' + title.text + '"' + ',' + priceIncl.text[2:] + ',' +
+                   priceExcl.text[2:] + ',' + str(nbr) + ',' + '"' + description.text.replace('"', '""') + '"' + ',' +
+                   bookCategory.text[1:-1] + ',' + ' '.join(rating)[12:] + ',' +
+                   'http://books.toscrape.com/' + urlImg + '\n')
 
-        for row in inf:
-            url = row.strip()
-            responseBooks = requests.get(url)
 
-            if responseBooks.ok:
-                soup = BeautifulSoup(responseBooks.text, 'html.parser')
-                trs = soup.findAll('tr')
+url = 'http://books.toscrape.com/'
+response = requests.get(url)
 
-                upc = trs[0].find('td')
-                priceIncl = trs[3].find('td')
-                priceExcl = trs[2].find('td')
+if response.ok:
+    soupCtg = BeautifulSoup(response.text, 'html.parser')
+    menuCtg = soupCtg.findAll('ul', {'class': 'nav nav-list'})
+    categories = menuCtg[0].find('ul').findAll('li')
 
-                nbrAvailable = trs[5].find('td')
-                nbr = int(''.join(filter(str.isdigit, nbrAvailable.text)))
+    for category in categories:
+        i = 0
 
-                title = soup.find('h1')
-                description = soup.find('p', {'class': ''})
+        a = category.find('a')
+        link = a['href']
+        links = url + link
 
-                category = soup.findAll('li')[2]
-                rating = soup.find('p', {'class': 'star-rating'}).get('class')
+        urlCategories = links[0:-10]
+        responseBooks = requests.get(urlCategories)
 
-                urlImgRaw = soup.find('div', {'class': 'item active'}).find('img')
-                urlImg = urlImgRaw.get('src')[6:]
+        if responseBooks.ok:
+            soupBooks = BeautifulSoup(responseBooks.text, 'html.parser')
+            urlBooks = soupBooks.findAll('div', {'class': 'image_container'})
 
-                outf.write(url + ',' + upc.text + ',' + '"' + title.text + '"' + ',' + priceIncl.text[1:] + ',' +
-                           priceExcl.text[1:] + ',' + str(nbr) + ',' + '"' + description.text.replace('"', '""') + '"' + ',' +
-                           category.text[1:-1] + ',' + ' '.join(rating)[12:] + ',' +
-                           'http://books.toscrape.com/' + urlImg + '\n')
+            classNext = soupBooks.find('li', {'class': 'next'})
+            nameCategory = urlCategories[51:-1].strip('_0123456789')
+
+            if not classNext:
+                with open('csv/' + nameCategory + '.csv', 'w', encoding='ISO-8859-1') as outp:
+                    booksFile = csv.writer(outp, delimiter=',', quoting=csv.QUOTE_NONE, quotechar="")
+                    outp.write('product_page_url, universal_product_code, title, '
+                               'price_including_tax, price_excluding_tax, number_available,'
+                               'product_description, category, review_rating, image_url\n')
+
+                    getContent()
+
+            else:
+                with open('csv/' + nameCategory + '.csv', 'w', encoding='ISO-8859-1') as outp:
+                    booksFile = csv.writer(outp, delimiter=',', quoting=csv.QUOTE_NONE, quotechar="")
+                    outp.write('product_page_url, universal_product_code, title, '
+                               'price_including_tax, price_excluding_tax, number_available,'
+                               'product_description, category, review_rating, image_url\n')
+                    while True:
+                        i += 1
+                        urlPages = urlCategories + 'page-' + str(i) + '.html'
+                        responsePages = requests.get(urlPages)
+
+                        if responsePages.ok:
+                            soupPages = BeautifulSoup(responsePages.text, 'html.parser')
+                            urlBooks = soupPages.findAll('div', {'class': 'image_container'})
+
+                            getContent()
+
+                        if not responsePages.ok:
+                            break
