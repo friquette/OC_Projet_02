00c419cc2f71e339086375c13cd555a0f934891e
Thomas Fricard <thomasfricard@gmail.com>
2020-11-12 14:13:10 +0100

    découpage de la fonction getContent en 3 sous fonctions: getContent : récupère le contenu d'un produit dlImage : enregistre l'image produit writeFile : écrit le contenu de getContent dans le fichier csv

--- script.py
+++ script.py
@@ -4,9 +4,9 @@ import csv
 import os.path
 import shutil
 
-with open('config.txt', 'r') as conf:
+with open('config.txt', 'r') as conf:               #lecture du fichier config.txt pour récupérer le chemin de dossier
     csvFolder = conf.read().strip(' \n')
-    if csvFolder == '':
+    if csvFolder == '':                             #si le fichier est vide, le chemin par défaut est à la racine du projet
         currentFolder = os.getcwd()
         csvFolder = currentFolder + '/csv'
         if not os.path.exists(currentFolder):
@@ -17,51 +17,53 @@ with open('config.txt', 'r') as conf:
 
 
 def getContent(myPath):
-    for urlBook in urlBooks:
-        aBooks = urlBook.find('a')
-        linkBook = aBooks['href']
-        linkBooks = url + 'catalogue/' + linkBook[9:]
+    responseContent = requests.get(linkBooks)
+    soupContent = BeautifulSoup(responseContent.text, 'html.parser')
 
-        responseContent = requests.get(linkBooks)
-        soupContent = BeautifulSoup(responseContent.text, 'html.parser')
+    trs = soupContent.findAll('tr')
+    upc = trs[0].find('td')
+    priceIncl = trs[3].find('td')
+    priceExcl = trs[2].find('td')
 
-        trs = soupContent.findAll('tr')
-        upc = trs[0].find('td')
-        priceIncl = trs[3].find('td')
-        priceExcl = trs[2].find('td')
+    nbrAvailable = trs[5].find('td')
+    nbr = int(''.join(filter(str.isdigit, nbrAvailable.text)))
 
-        nbrAvailable = trs[5].find('td')
-        nbr = int(''.join(filter(str.isdigit, nbrAvailable.text)))
+    title = soupContent.find('h1')
 
-        title = soupContent.find('h1')
+    description = soupContent.find('p', {'class': ''})
+    if description:
+        descriptionBook = description.text
+    else:
+        descriptionBook = ''
 
-        description = soupContent.find('p', {'class': ''})
-        if description:
-            descriptionBook = description.text
-        else:
-            descriptionBook = ''
+    bookCategory = soupContent.findAll('li')[2]
+    rating = soupContent.find('p', {'class': 'star-rating'}).get('class')
 
-        bookCategory = soupContent.findAll('li')[2]
-        rating = soupContent.find('p', {'class': 'star-rating'}).get('class')
+    urlImgRaw = soupContent.find('div', {'class': 'item active'}).find('img')
+    urlImg = urlImgRaw.get('src')[6:]
 
-        urlImgRaw = soupContent.find('div', {'class': 'item active'}).find('img')
-        urlImg = urlImgRaw.get('src')[6:]
+    urlImgFull = url + urlImg
+    responseImage = requests.get(urlImgFull, stream=True)
+    imageName = urlImgFull.split('/')[-1]
+    imagePath = myPath + '/' + imageName
 
-        outp.write(linkBooks + ',' + upc.text + ',' + '"' + title.text + '"' + ',' + priceIncl.text[2:] + ',' +
-                   priceExcl.text[2:] + ',' + str(nbr) + ',' + '"' + descriptionBook.replace('"', '""') + '"' + ',' +
-                   bookCategory.text[1:-1] + ',' + ' '.join(rating)[12:] + ',' +
-                   'http://books.toscrape.com/' + urlImg + '\n')
+    return (linkBooks, upc, title, priceIncl, priceExcl, nbr, descriptionBook, bookCategory,
+            rating, urlImg, myPath, responseImage, imagePath)
 
-        urlImgFull = url + urlImg
-        responseImage = requests.get(urlImgFull, stream=True)
-        imageName = urlImgFull.split('/')[-1]
 
-        if responseImage.ok:
-            responseImage.raw.decode_content = True
-            with open(myPath + '/' + imageName, 'wb') as f:
-                shutil.copyfileobj(responseImage.raw, f)
-        else:
-            print('Image not found')
+def dlImage(func):
+    if func[11].ok:
+        func[11].raw.decode_content = True
+        with open(func[12], 'wb') as f:
+            shutil.copyfileobj(func[11].raw, f)
+    else:
+        print('Image not found')
+
+
+def writeFile(nameOutput, func):
+    nameOutput.write(func[0] + ',' + func[1].text + ',' + '"' + func[2].text + '"' + ',' + func[3].text[2:] + ',' +
+                     func[4].text[2:] + ',' + str(func[5]) + ',' + '"' + func[6].replace('"', '""') + '"' + ',' +
+                     func[7].text[1:-1] + ',' + ' '.join(func[8])[12:] + ',' + func[12] + '\n')
 
 
 url = 'http://books.toscrape.com/'
@@ -70,14 +72,14 @@ response = requests.get(url)
 if response.ok:
     soupCtg = BeautifulSoup(response.text, 'html.parser')
     menuCtg = soupCtg.findAll('ul', {'class': 'nav nav-list'})
-    categories = menuCtg[0].find('ul').findAll('li')
+    categories = menuCtg[0].find('ul').findAll('li')                #récupération de toutes les catégories
 
     for category in categories:
         i = 0
 
         a = category.find('a')
         link = a['href']
-        links = url + link
+        links = url + link                  #url complète de chaque catégorie dans la variable links
 
         urlCategories = links[0:-10]
         responseBooks = requests.get(urlCategories)
@@ -89,7 +91,7 @@ if response.ok:
             classNext = soupBooks.find('li', {'class': 'next'})
             nameCategory = urlCategories[51:-1].strip('_0123456789')
 
-            if not classNext:
+            if not classNext:                                   #si la catégorie n'a qu'une seule page
                 categoryPath = csvFolder + '/' + nameCategory
                 if not os.path.exists(categoryPath):
                     os.makedirs(categoryPath)
@@ -100,9 +102,14 @@ if response.ok:
                                'price_including_tax, price_excluding_tax, number_available,'
                                'product_description, category, review_rating, image_url\n')
 
-                    getContent(categoryPath)
+                    for urlBook in urlBooks:
+                        aBooks = urlBook.find('a')
+                        linkBook = aBooks['href']
+                        linkBooks = url + 'catalogue/' + linkBook[9:]
 
-            else:
+                        writeFile(outp, getContent(categoryPath))
+                        dlImage(getContent(categoryPath))
+            else:                                               #si la catégorie a plusieurs pages
                 categoryPath = csvFolder + '/' + nameCategory
                 if not os.path.exists(categoryPath):
                     os.mkdir(categoryPath)
@@ -114,14 +121,20 @@ if response.ok:
                                'product_description, category, review_rating, image_url\n')
                     while True:
                         i += 1
-                        urlPages = urlCategories + 'page-' + str(i) + '.html'
+                        urlPages = urlCategories + 'page-' + str(i) + '.html'   #tant que request renvoie une réponse on incrémente le numéro de la page dans l'url
                         responsePages = requests.get(urlPages)
 
                         if responsePages.ok:
                             soupPages = BeautifulSoup(responsePages.text, 'html.parser')
                             urlBooks = soupPages.findAll('div', {'class': 'image_container'})
 
-                            getContent(categoryPath)
+                            for urlBook in urlBooks:
+                                aBooks = urlBook.find('a')
+                                linkBook = aBooks['href']
+                                linkBooks = url + 'catalogue/' + linkBook[9:]
+
+                                writeFile(outp, getContent(categoryPath))
+                                dlImage(getContent(categoryPath))
 
                         if not responsePages.ok:
                             break
